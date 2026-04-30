60bfb4cbfeea76541220ebe33920515819d0fb10
Nadia Rahman <sc23n2r@leeds.ac.uk>
2026-04-30 12:49:30 +0100

    add phrase no-common-pages test, fix gitignore for pycache

--- src/search.py
+++ src/search.py
@@ -121,3 +121,14 @@ class Searcher:
 
         scored.sort(key=lambda x: x[1], reverse=True)
         return scored
+    
+def test_phrase_no_common_pages_returns_empty(self, capsys):
+        index = {
+            "hello": {"https://quotes.toscrape.com/page/1/": {"freq": 1, "positions": [0]}},
+            "world": {"https://quotes.toscrape.com/page/2/": {"freq": 1, "positions": [0]}},
+        }
+        searcher = Searcher(index)
+        results = searcher.find_phrase(["hello", "world"])
+        assert results == []
+        captured = capsys.readouterr()
+        assert "No pages found" in captured.out
